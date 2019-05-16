cddb4f4727e1b21040e72a2362ffc433c0ff910b
Ligia Almeida Silva <ligiaalmeida@metodologiagb.com.br>
2019-05-16 18:15:02 -0300

    alteração manifest.json e add service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,8 @@
+const CACHE_NAME = 'pwa-cache-v1';
+
+const urlsToCache = [
+  "/",
+  "static/js/bundle.js",
+  'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
+  'https://5a9dd5a1a65e7d0014436b95.mockapi.io/note'
+]
